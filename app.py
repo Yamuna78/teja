401b28697ef0d6b29d16a60f4347e28709c0311e
Yamuna78 <p.yamuna101@gmail.com>
2025-03-03 15:51:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import *
+import os
 
-app = Flask(__name__)
+
+
+app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Portfolio Page
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('index.html')
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+app.run(debug=True)
